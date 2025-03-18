f79f26b89339ffa6283f34095deaf46b4de16cb1
avaAlley <ava.rose.alley@gmail.com>
2025-03-18 20:40:48 +0000

    fizzbuzz start

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -10,10 +10,33 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
 */
+
+/*
+
+I: nothing
+O: print Fizz for multiples of 3, buzz for multiples of 5, and multiples of both FizzBuzz, others log number
+C: 
+E: 
+
+*/
+
+
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
+    // declare variable to 1 to increment
+    var willFizzBuzz = 1
+    // while variable is < 100 loop
+    // while (willFizzBuzz <= 100){
+        
+    }
+    // conditional statement for multiple of 3 AND 5
 
+    // conditional for multiples of 3
+
+    // conditional for multiples of 5
+
+    // else print number
 
     // YOUR CODE GOES ABOVE HERE //
 }
